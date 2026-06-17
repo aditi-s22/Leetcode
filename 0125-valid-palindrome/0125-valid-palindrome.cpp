9bd88f8a65d93a0d0415c56c62e780d48f1e1943
aditi-s22 <156512463+aditi-s22@users.noreply.github.com>
2026-06-17 15:18:34 +0530

    Time: 7 ms (5.6%), Space: 10.2 MB (24.63%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        string t = "";
+
+        for (char ch : s) {
+
+            if (isalnum(ch)) {
+
+                t += tolower(ch);
+            }
+        }
+
+        int i = 0;
+        int j = t.size() - 1;
+
+        while (i < j) {
+
+            if (t[i] != t[j])
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
